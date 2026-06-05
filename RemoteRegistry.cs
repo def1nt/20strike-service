b79c576b0dcf22e44c5891743bb280f3904a4c0f
def1nt <def1nt@mail.ru>
2026-06-05 15:26:53 +0300

    safer work with unmanaged registry resources

--- RemoteRegistry.cs
+++ RemoteRegistry.cs
@@ -4,48 +4,53 @@ namespace _20strike;
 
 partial class Application
 {
+    static readonly string[] POIKeys = ["DisplayName", "DisplayVersion", "EstimatedSize", "InstallDate", "InstallLocation", "Publisher", "URLInfoAbout"];
+
     private List<SoftwareInfo> PollSoftware(string computername)
     {
         if (!OperatingSystem.IsWindows()) return [];
         string path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
         string path64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
-        RegistryKey anotherkey;
         try
         {
-            anotherkey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, computername);
+            List<SoftwareInfo> softwareinfo = [];
+
+            using RegistryKey HKLMRootKey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, computername);
+            {
+                DBCleanup(computername, "Meta_Software");
+                using RegistryKey? key32 = HKLMRootKey.OpenSubKey(path);
+                if (key32 is not null) softwareinfo = AddToDB(key32, computername);
+
+                using RegistryKey? key64 = HKLMRootKey.OpenSubKey(path64);
+                if (key64 is not null) softwareinfo.AddRange(AddToDB(key64, computername));
+            }
+
+            using RegistryKey HKURootKey = RegistryKey.OpenRemoteBaseKey(RegistryHive.Users, computername);
+            {
+                var users = HKURootKey.GetSubKeyNames();
+                foreach (var user in users)
+                {
+                    try
+                    {
+                        using RegistryKey? usertree = HKURootKey.OpenSubKey(user);
+                        if (usertree == null) continue;
+                        using RegistryKey? key = usertree.OpenSubKey(path);
+                        if (key != null) softwareinfo.AddRange(AddToDB(key, computername));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"ERROR: User {user} registry access failed: {e.Message}");
+                        continue;
+                    }
+                }
+            }
+            return softwareinfo;
         }
         catch (Exception e)
         {
             if (e is IOException) { Console.WriteLine($"ERROR: {e.Message}"); return []; }
             throw;
         }
-        DBCleanup(computername, "Meta_Software");
-        RegistryKey? key = anotherkey.OpenSubKey(path);
-        List<SoftwareInfo> softwareinfo = [];
-        if (key != null) softwareinfo = AddToDB(key, computername);
-
-        anotherkey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, computername);
-        key = anotherkey.OpenSubKey(path64);
-        if (key != null) softwareinfo = [.. softwareinfo, .. AddToDB(key, computername)];
-
-        anotherkey = RegistryKey.OpenRemoteBaseKey(RegistryHive.Users, computername);
-        var users = anotherkey.GetSubKeyNames();
-        foreach (var user in users)
-        {
-            try
-            {
-                using RegistryKey? usertree = anotherkey.OpenSubKey(user);
-                if (usertree == null) continue;
-                key = usertree.OpenSubKey(path);
-                if (key != null) softwareinfo = [.. softwareinfo, .. AddToDB(key, computername)];
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"ERROR: {e.Message}");
-                continue;
-            }
-        }
-        return softwareinfo;
     }
 
     private List<SoftwareInfo> AddToDB(RegistryKey rk, string computername)
@@ -59,14 +64,13 @@ partial class Application
             using RegistryKey? softwareinfo = rk.OpenSubKey(software);
             if (softwareinfo == null) continue;
 
-            string[] POIKeys = ["DisplayName", "DisplayVersion", "EstimatedSize", "InstallDate", "InstallLocation", "Publisher", "URLInfoAbout"];
-
             if (unnull(softwareinfo.GetValue("DisplayName")) == "") continue;
             Dictionary<string, string> softwareRecordTemp = [];
             foreach (string key in POIKeys)
             {
-                softwareRecordTemp.Add(key, unnull(softwareinfo.GetValue(key)));
-                DBInsert([computername, "Meta_Software", key, "String", unnull(softwareinfo.GetValue(key))]);
+                string value = unnull(softwareinfo.GetValue(key));
+                softwareRecordTemp.Add(key, value);
+                DBInsert([computername, "Meta_Software", key, "String", value]);
             }
             softwareinfo_.Add(new()
             {
